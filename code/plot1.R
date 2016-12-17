d846426367dae5a48dc2c685622320cec1bd2136
Lokesh30996 <lokesh.borra30996@gmail.com>
2016-12-17 07:32:39 +0530

    final commit

--- code/plot1.R
+++
@@ -1,4 +0,0 @@
-power.consumption<-data.table(read.csv("cleaned.csv"))
-png("plot1.png",width=480,height = 480,units="px")
-hist(power.consumption$GlobalActivePower,col="red",xlab = "Global Active Power (kilowatts)")
-x<-dev.off()
--- code/plot2.R
+++
@@ -1,4 +0,0 @@
-colnames(power.consumption)
-png("plot2.png",width=480,height = 480,units="px")
-plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
-x<-dev.off()
